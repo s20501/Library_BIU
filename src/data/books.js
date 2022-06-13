33353526e0189a1174e67fda126f15604df798de
Marcin muda-Trzebiatowski <s20501@pjwstk.edu.pl>
2022-06-13 20:50:23 +0200

    Book details

--- src/data/books.js
+++ src/data/books.js
@@ -1,7 +1,7 @@
 const books = [
 
-    { id: 1, name: "Potop", ISBN: "312312" },
-    { id: 2, name: "Ogniem i mieczem", ISBN: "3240902" }
+    { id: 1, name: "Potop", ISBN: "312312", author: "Adam Bogdan", term: "", owner: "" },
+    { id: 2, name: "Ogniem i mieczem", ISBN: "3240902", author: "Adam Bogdan", term: "12.12.2022", owner: "Admin" }
 
 ];
 
--- src/views/Book.js
+++ src/views/Book.js
@@ -1,9 +1,58 @@
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    Row,
+    Col,
+} from "reactstrap";
+import Header from "components/Headers/Header.js";
+import booksData from "data/books.js"
+import { useState } from "react/cjs/react.production.min";
+
+
 const { useParams } = require("react-router-dom");
+
+
 
 const Book = () => {
 
     let { id } = useParams();
-    return <h3>Requested book ID: {id}</h3>;
+    const book = booksData.find(b => b.id === parseInt(id));
+
+    return (
+        <>
+            <Header />
+            <Card className="card-profile shadow">
+
+                <CardBody className="pt-0 pt-md-4">
+
+                    <div className="text-center">
+                        <h3>
+                            {book.name}, {book.ISBN}
+                            <span className="font-weight-light"></span>
+                        </h3>
+                        <div className="h5 font-weight-300">
+                            <i className="ni location_pin mr-2" />
+                            {book.author}
+                        </div>
+
+                        <div className="h5 font-weight-300">
+                            <i className="ni location_pin mr-2" />
+                            {book.term && `Term: ${book.term}`}
+
+                        </div>
+
+
+                        <hr className="my-4" />
+
+                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                            {book.term ? 'Return' : 'Borrow'}
+                        </a>
+                    </div>
+                </CardBody>
+            </Card>
+        </>);
 }
 
 export default Book
