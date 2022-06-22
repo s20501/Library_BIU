5d20161a21704dd7629e91e47c3349f4ff8e3164
Marcin muda-Trzebiatowski <s20501@pjwstk.edu.pl>
2022-06-22 21:28:08 +0200

    Sidebar clean up

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,44 +1,22 @@
 
-/*eslint-disable*/
-import { useState } from "react";
 import { NavLink as NavLinkRRD } from "react-router-dom";
-// nodejs library to set properties for components
 import { PropTypes } from "prop-types";
 
 // reactstrap components
 import {
   Collapse,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
   NavbarBrand,
   Navbar,
   NavItem,
   NavLink,
   Nav,
   Container,
-  Row,
-  Col,
 } from "reactstrap";
 
 
 const Sidebar = (props) => {
-  const [collapseOpen, setCollapseOpen] = useState();
-  // toggles collapse between opened and closed (true/false)
-  const toggleCollapse = () => {
-    setCollapseOpen((data) => !data);
-  };
-  // closes the collapse
-  const closeCollapse = () => {
-    setCollapseOpen(false);
-  };
-  // creates the links that appear in the left menu / Sidebar
+
+
   const createLinks = (routes) => {
     return routes.filter(r => !r.hideNav).map((prop, key) => {
       return (
@@ -46,7 +24,6 @@ const Sidebar = (props) => {
           <NavLink
             to={prop.layout + prop.path}
             tag={NavLinkRRD}
-            onClick={closeCollapse}
             activeClassName="active"
           >
             <i className={prop.icon} />
@@ -66,61 +43,15 @@ const Sidebar = (props) => {
       id="sidenav-main"
     >
       <Container fluid>
-        {/* Toggler */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={toggleCollapse}
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        {/* Brand */}
+
 
         <NavbarBrand className="pt-0">
           Library
         </NavbarBrand>
 
-        {/* User */}
-        <Nav className="align-items-center d-md-none">
-          <UncontrolledDropdown nav>
-            <DropdownToggle nav className="nav-link-icon">
-              <i className="ni ni-bell-55" />
-            </DropdownToggle>
-            <DropdownMenu
-              aria-labelledby="navbar-default_dropdown_1"
-              className="dropdown-menu-arrow"
-              right
-            >
-              <DropdownItem>Action</DropdownItem>
-              <DropdownItem>Another action</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Something else here</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
         {/* Collapse */}
-        <Collapse navbar isOpen={collapseOpen}>
-          {/* Collapse header */}
-          <div className="navbar-collapse-header d-md-none">
-            <Row>
-
-              <Col className="collapse-close" xs="6">
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  onClick={toggleCollapse}
-                >
-                  <span />
-                  <span />
-                </button>
-              </Col>
-            </Row>
-          </div>
-          {/* Form */}
-
-          {/* Navigation */}
+        <Collapse navbar>
           <Nav navbar>{createLinks(routes)}</Nav>
-          {/* Divider */}
           <hr className="my-3" />
         </Collapse>
       </Container>
@@ -133,20 +64,8 @@ Sidebar.defaultProps = {
 };
 
 Sidebar.propTypes = {
-  // links that will be displayed inside the component
   routes: PropTypes.arrayOf(PropTypes.object),
-  logo: PropTypes.shape({
-    // innerLink is for links that will direct the user within the app
-    // it will be rendered as <Link to="...">...</Link> tag
-    innerLink: PropTypes.string,
-    // outterLink is for links that will direct the user outside the app
-    // it will be rendered as simple <a href="...">...</a> tag
-    outterLink: PropTypes.string,
-    // the image src of the logo
-    imgSrc: PropTypes.string.isRequired,
-    // the alt for the img
-    imgAlt: PropTypes.string.isRequired,
-  }),
+
 };
 
 export default Sidebar;
